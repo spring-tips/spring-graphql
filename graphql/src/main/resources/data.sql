7874752b7213d6b29d6150f07f80bdb9f0175010
Josh Long <josh@joshlong.com>
2021-08-17 02:08:51 -0700

    graphql

---
+++ graphql/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into customer(name) values('Madhura');
+insert into customer(name) values('Yuxin');
+insert into customer(name) values('Stéphane');
+insert into customer(name) values('Olga');
+insert into customer(name) values('Violetta');
+insert into customer(name) values('Dr. Syer');
+insert into customer(name) values('Spencer');
+insert into customer(name) values('Josh');
